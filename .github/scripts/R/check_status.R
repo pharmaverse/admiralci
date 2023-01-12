d138e392165f65726b586ea7878ed1ae36fe47e6
Grasselly <grasseld@emea.roche.com>
2023-01-12 12:26:51 +0100

    update cran status based on errors details md5

--- .github/scripts/R/check_status.R
+++ .github/scripts/R/check_status.R
@@ -4,6 +4,24 @@ library(rvest)
 library(stringr)
 library(digest)
 
+# check if needed : package name and working dir path as input arguments :
+#library("optparse")
+#option_list = list(
+#  make_option(c("-w", "--working_dir_path"), type="character", default=NULL, 
+#              help="working dir", metavar="character"),
+#    make_option(c("-p", "--package_name"), type="character", default=NULL, 
+#              help="Current package name", metavar="character")
+#); 
+#opt_parser = OptionParser(option_list=option_list);
+#opt = parse_args(opt_parser);
+
+
+parseErrors <- function(url) {
+    return(
+        tryCatch(url %>%  read_html() %>% html_text(), error=function(e) "URL Not Found")
+    )
+}   
+
 pkg <- paste(desc::desc_get(keys="Package"))
 url <- sprintf("https://cran.r-project.org/web/checks/check_results_%s.html", pkg)
 if (!httr::http_error(url)) {
@@ -18,10 +36,14 @@ if (!httr::http_error(url)) {
     errors <- filter(checks, Status  == 'ERROR')
     errors$Links <- str_c('https://www.r-project.org/nosvn/R.check/', errors$Flavor, '/', pkg, 
     '-00install.html') # TODO: see dinakar if we should go through each detail URI (check, install steps etc)
-    errors$Details <- lapply(errors$ErrorLinks, FUN=function(x) x %>%  read_html() %>% html_text())
+    errors$Details <- lapply(errors$Links, FUN=parseErrors)
     errors$Id <- lapply(errors$Details, digest)
-    #FIXME: wrong json format..
-    ids <- errors %>% select(Flavor, Id) %>% toJSON(dataframe = 'values', pretty = T) 
+
+    # Alphanumeric order on Flavor (for cran status comparison)
+    errors <- errors[order(errors$Flavor),] 
+
+    # Save into CSV: 
+    errors %>% select(Flavor, Id) %>% write.csv('cran_errors.csv',row.names=FALSE)
             
     status_types <- "${{ inputs.status-types }}"
     statuses <- unlist(strsplit(status_types, split = ","))
@@ -39,6 +61,6 @@ if (!httr::http_error(url)) {
         cran_status(sprintf(
             "\n\nAll details and logs are available here: %s", url
         ))
-        stop("❌ One or more CRAN checks resulted in an invalid status ❌")
+        print("❌ One or more CRAN checks resulted in an invalid status ❌")
         }
 }
