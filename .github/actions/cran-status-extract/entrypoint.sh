74a2726cea7e41ee1bce74ea3edc765500741dd0
Daphné Grasselly <daphne.grasselly@orange.fr>
2023-01-23 10:14:08 +0100

    fix R path entrypoint

--- .github/actions/cran-status-extract/entrypoint.sh
+++ .github/actions/cran-status-extract/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-Rscript ./check-status.R --status_types "${INPUT_STATUS_TYPES}"
+Rscript /app/check-status.R --status_types "${INPUT_STATUS_TYPES}"
 
 # change underscore to dash
