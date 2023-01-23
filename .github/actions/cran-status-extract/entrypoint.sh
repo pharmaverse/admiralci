adad6f5b0eff8884cf4dc7c61e9b1db03d3176da
Daphné Grasselly <daphne.grasselly@orange.fr>
2023-01-23 14:23:37 +0100

    change input var yml with undescore

--- .github/actions/cran-status-extract/entrypoint.sh
+++ .github/actions/cran-status-extract/entrypoint.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-echo "${INPUT_STATUS-TYPES}"
-Rscript /app/check-status.R --status_types "${INPUT_STATUS-TYPES}"
+Rscript /app/check-status.R --status_types "${INPUT_STATUS_TYPES}"
