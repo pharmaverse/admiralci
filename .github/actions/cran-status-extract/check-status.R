82b7e7eaeeb98fda9dc9bf35c915d1d54f9abc13
Daphné Grasselly <daphne.grasselly@orange.fr>
2023-01-24 08:48:25 +0100

    clean R code

--- .github/actions/cran-status-extract/check-status.R
+++ .github/actions/cran-status-extract/check-status.R
@@ -44,7 +44,6 @@ build_md5_codes <- function(pkg, errors, step) {
 }
 
 pkg <- paste(desc::desc_get(keys = "Package"))
-pkg <- "assignR" # TODO: del this line
 url <- sprintf("https://cran.r-project.org/web/checks/check_results_%s.html", pkg)
 
 if (!httr::http_error(url)) {
