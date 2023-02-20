7ac3d7ee3a767d02a5f3b40690429b818581bfd4
Adam Forys <adam.forys@roche.com>
2023-02-20 14:11:07 +0100

    Fix links, cleanup onLoad

--- R/admiralci-package.R
+++ R/admiralci-package.R
@@ -1,6 +1,36 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' Tweak yml url
+#' @param ... Arguments from pkgdown tweak_page
+#' @export
+tweak_yml_url <- function(...) {
+  html <- ..1
+
+  links <- xml2::xml_find_all(html, ".//a")
+  if (length(links) == 0)
+    return(invisible())
+
+  hrefs <- xml2::xml_attr(links, "href")
+  needs_tweak <- grepl("\\.yml$", hrefs) & xml2::url_parse(hrefs)$scheme == ""
+
+  fix_links <- function(x) {
+    x <- gsub("^./", "", x)
+    x <- paste0("https://github.com/pharmaverse/admiralci/blob/HEAD/", x)
+  }
+
+  if (any(needs_tweak)) {
+    purrr::walk2(
+      links[needs_tweak],
+      fix_links(hrefs[needs_tweak]),
+      xml2::xml_set_attr,
+      attr = "href"
+    )
+  }
+
+  invisible()
+}
+
 #' onLoad function
 #'
 #' This function is called automatically during package loading.
@@ -11,31 +41,6 @@
 .onLoad <- function(libname, pkgname) { # nolint
 
   # Tweak page with special custom hook.
-  setHook("UserHook::admiralci::tweak_page", function(...) {
-    html <- ..1
-
-    links <- xml2::xml_find_all(html, ".//a")
-    if (length(links) == 0)
-      return(invisible())
-
-    hrefs <- xml2::xml_attr(links, "href")
-    needs_tweak <- grepl("\\.yml$", hrefs) & xml2::url_parse(hrefs)$scheme == ""
-
-    fix_links <- function(x) {
-      x <- gsub("^./", "", x)
-      x <- paste0("https://github.com/pharmaverse/admiralci/blob/HEAD/", x)
-    }
-
-    if (any(needs_tweak)) {
-      purrr::walk2(
-        links[needs_tweak],
-        fix_links(hrefs[needs_tweak]),
-        xml2::xml_set_attr,
-        attr = "href"
-      )
-    }
-
-    invisible()
-  })
+  setHook("UserHook::admiralci::tweak_page", tweak_yml_url)
 
 }
