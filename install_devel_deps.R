a25b02bc9dff7a7cb1a52693c8f0a807a3c4bc79
Daphné Grasselly <56442075+dgrassellyb@users.noreply.github.com>
2024-01-16 14:11:22 +0100

    Remove renv and use cran latest (#176)
    * add vignettes
    
    * add doc
    
    * use cran latest
    
    * temp comments
    
    * use tmp branch
    
    * fix
    
    * fix
    
    * fix
    
    * add supp packages
    
    * fix typo
    
    * update docker images
    
    * add needs
    
    * fix im name
    
    * update workflow to use latest im
    
    * fix
    
    * delete rprofile
    
    * fix deps name
    
    * add old release
    
    * add old release check
    
    * fix typo
    
    * fix release artifacts
    
    * update install deps script & lintr
    
    * fix link
    
    * fix link
    
    * rename latest image into release
    
    * Update push-docker-image.yml
    
    * install suggest deps
    
    * add step to install deps - r cmd check
    
    * add other r cmd check job
    
    * fix indent
    
    * fix
    
    * try p3m cran repo for devel
    
    * try another cran repo devel
    
    * update boilerplate steps with new deps, upload deps artifacts
    
    * add r_version to artifact
    
    * fix artifact name
    
    * trigger checks only on dispatch, generate renv.lock on artifacts
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix artifacts multiple paths
    
    * fix path
    
    * fix
    
    * retry
    
    * fix renv artifact names
    
    ---------
    
    Co-authored-by: pharmaverse-bot <113703390+pharmaverse-bot@users.noreply.github.com>
    Co-authored-by: cicdguy <26552821+cicdguy@users.noreply.github.com>

--- install_devel_deps.R
+++ install_devel_deps.R
@@ -1,9 +1,19 @@
 # Read the dependencies from the CSV file
-dependencies <- read.csv("deps.csv", stringsAsFactors = FALSE)
+dependencies <- read.csv("Dependencies.list.of.docker.image.admiralci-latest.csv",
+  stringsAsFactors = FALSE)
+
+# remove built-in deps from the list
+dependencies <- dependencies %>% filter(LibPath != "/usr/local/lib/R/library")
 
 # Loop through the dependencies and reinstall packages with specific versions
 for (i in seq_len(nrow(dependencies))) {
   dep_name <- dependencies[i, "Package"]
   dep_version <- dependencies[i, "Version"]
-  install.packages(dep_name, type = "source", repos = NULL, version = dep_version)
+  version_parts <- strsplit(dep_version, "\\.")[[1]]
+  if (length(version_parts) == 4) {
+    print(sprintf("skipping installation of dep %s", dep_name))
+  } else {
+    print(sprintf("install dependency %s - version %s", dep_name, dep_version))
+    devtools::install_version(dep_name, version = dep_version, repos = "https://cran.r-project.org", force=TRUE, upgrade="always")
+  }
 }
